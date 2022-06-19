910f08261893c4c06dc419d7a3913db248d8f4c9
Maksym-Semenov <mestcomua@gmail.com>
2022-06-19 21:37:12 +0300

    Добавьте файлы проекта.

---
+++ DelegatsAndEvents/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DelegatsAndEvents;
+
+string testString = "abracadabra";
+
+StringHelper helper = new StringHelper();
+CountDelegate d1 = helper.GetCount;
+CountDelegate d2 = helper.GetCountSymbol;
+
+Console.WriteLine("Общее кол-во символов {0}", TestMetod (d1, testString));
+Console.WriteLine("Общее кол-во символов A {0}", TestMetod(d2, testString)); ;
+
+
+static int TestMetod(CountDelegate method, string testString)
+{
+    return method(testString);
+}
---
+++ DelegatsAndEvents/StringHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace DelegatsAndEvents
+{
+    public delegate int CountDelegate(string message);
+    internal class StringHelper
+    {
+        public int GetCount(string inputString)
+        {
+            return inputString.Length;
+        }
+        public int GetCountSymbol(string inputString)
+        {
+            return inputString.Count(c => c=='a');
+        }
+    }
+}
