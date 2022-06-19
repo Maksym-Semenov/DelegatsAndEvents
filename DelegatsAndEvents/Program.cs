c489fa7519dadea9d84e2141d179311332f43438
Maksym-Semenov <mestcomua@gmail.com>
2022-06-19 21:42:36 +0300

    update

--- DelegatsAndEvents/Program.cs
+++ DelegatsAndEvents/Program.cs
@@ -7,11 +7,11 @@ StringHelper helper = new StringHelper();
 CountDelegate d1 = helper.GetCount;
 CountDelegate d2 = helper.GetCountSymbol;
 
-Console.WriteLine("Общее кол-во символов {0}", TestMetod (d1, testString));
-Console.WriteLine("Общее кол-во символов A {0}", TestMetod(d2, testString)); ;
+Console.WriteLine("Общее кол-во символов {0}", TestMethod (d1, testString));
+Console.WriteLine("Общее кол-во символов A {0}", TestMethod(d2, testString)); ;
 
 
-static int TestMetod(CountDelegate method, string testString)
+static int TestMethod(CountDelegate method, string testString)
 {
     return method(testString);
 }
